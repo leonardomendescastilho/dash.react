7c55229c70fb8d355840ad46155e18b9cfefc24b
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-22 12:25:37 -0300

    feat: create the design login page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 function AuthLayout() {
   return (
-    <div>
-      <h1>Autentificação</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-bold">pizza.shop</span>
+        </div>
+        <footer className="text-center text-sm">
+          Painel do parcerio &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
