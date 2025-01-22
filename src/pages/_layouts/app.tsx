8f7e1c7c16ef4f157cc7fd9659b3575e98571680
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-22 17:39:42 -0300

    feat: add Header component

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom'
 
+import Header from '@/components/header'
+
 function AppLayout() {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
-      <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <h1>
+        <Header />
+      </h1>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
