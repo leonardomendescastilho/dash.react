d332f8ec53ea3ac95a05313bce33b98b98b8271b
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-22 12:26:08 -0300

    feat: add the helmet

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,5 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
 function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div>Dashboard</div>
+    </>
+  )
 }
 
 export default Dashboard
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,41 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 function SignIn() {
-  return <div>SignIn</div>
+  return (
+    <>
+      <Helmet title="login" />
+      <div className="p-8">
+        <div className="flex w-[358px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parcerio{' '}
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Senha</Label>
+              <Input id="email" type="email" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default SignIn
