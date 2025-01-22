e584b5f95925a8b5954878e397cc8b4591d6c9c1
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-22 17:40:59 -0300

    feat: add the link component

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -39,7 +39,7 @@ function SignIn() {
 
   return (
     <>
-      <Helmet title="login" />
+      <Helmet title="Login" />
       <div className="p-8">
         <div className="flex w-[358px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
