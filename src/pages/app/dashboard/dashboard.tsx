d1ef441ef6d9d9064975167ee1970d41bf237e00
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-24 09:54:00 -0300

    feat: component dashboard

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
+function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div>Dashboard</div>
+    </>
+  )
+}
+
+export default Dashboard
