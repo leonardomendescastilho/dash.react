209f2f9c8805a530f8661c516f398b9410986826
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-01-24 16:44:19 -0300

    refactor: change the link to orders

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,7 +16,7 @@ function Header() {
             <Home className="h-4 w-4" />
             Início
           </NavLink>
-          <NavLink to={'/pedidos'}>
+          <NavLink to={'/orders'}>
             <UtensilsCrossed className="h-4 w-4" />
             Pedidos
           </NavLink>
