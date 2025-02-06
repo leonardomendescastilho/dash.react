c60e8bcd85b3cec3c088e3c427e76901e53b9612
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-02-06 11:23:00 -0300

    feat: apply useQuerie and fill with api response

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -9,8 +9,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { useQuery } from '@tanstack/react-query'
+import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 
 function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,14 +32,14 @@ function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop <ChevronDown className="h-4 w-4" />
+         {managedRestaurant?.name} <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Leonardo Mendes</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            leonardo@gmail.com
+          {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
