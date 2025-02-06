ef75ed6e0f3a7605adcc501532af58aa1730b62a
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-02-06 13:27:22 -0300

    feat: add the dialog

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -21,12 +21,14 @@ function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+    staleTime: Infinity,
   })
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
       queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
+      staleTime: Infinity,
     })
 
   return (
