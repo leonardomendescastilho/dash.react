1fb8c4da06e4bc63a5ffd0415f5264443b4b9521
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-02-09 16:49:17 -0300

    refactor: updateProfileMutation, apply onMutate and create updateManagedRestaurantCache fn to insta refresh the mutationValue bases on what has changed (dont need to doagain a request)

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,9 +1,14 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
-import { getManagedRestaurant, GetManagedRestaurantResponse } from '@/api/get-managed-restaurant'
+import {
+  getManagedRestaurant,
+  GetManagedRestaurantResponse,
+} from '@/api/get-managed-restaurant'
+import updateProfile from '@/api/update-profile'
 
 import { Button } from './ui/button'
 import {
@@ -17,12 +22,10 @@ import {
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
-import updateProfile from '@/api/update-profile'
-import { toast } from 'sonner'
 
 const storeProfileSchema = z.object({
   name: z.string().min(3),
-  description: z.string().nullable(),
+  description: z.string().nullable().optional(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -30,14 +33,17 @@ type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 function StoreProfileDialog() {
   const queryClient = useQueryClient()
 
-
   const { data: managedRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
   })
 
-  const { register, handleSubmit, formState: {isSubmitting} } = useForm<StoreProfileSchema>({
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileSchema>({
     resolver: zodResolver(storeProfileSchema),
     values: {
       name: managedRestaurant?.name ?? '',
@@ -45,25 +51,45 @@ function StoreProfileDialog() {
     },
   })
 
-  const  { mutateAsync: updateProfileMutation } = useMutation({
+  const updateManagedRestaurantCache = ({
+    name,
+    description,
+  }: StoreProfileSchema) => {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
+
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
+    }
+
+    return {cached}
+  }
+
+  const { mutateAsync: updateProfileMutation } = useMutation({
     mutationFn: updateProfile,
-    onSuccess: (_, {  name, description }) => {
-      const cache = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+    onMutate: ({ name, description }) => {
+     const { cached: previousData } = updateManagedRestaurantCache({ name, description })
 
-      if (cache) {
-        queryClient.setQueryData(['managed-restaurant'], {
-          ...cache,
-          name, 
-          description
-        })
-      }
+     return { previousData }
     },
+    onError: (_, __, context) => {
+      if (context?.previousData) {
+        updateManagedRestaurantCache(context.previousData)
+      }
+    }
   })
-
 
   const handleUpdateProfile = async (data: StoreProfileSchema) => {
     try {
-     await updateProfileMutation({
+      await updateProfileMutation({
         name: data.name,
         description: data.description ?? undefined,
       })
@@ -71,10 +97,9 @@ function StoreProfileDialog() {
     } catch (error) {
       toast.error('Erro ao atualizar o perfil da loja')
     }
-   
   }
 
-  return ( 
+  return (
     <>
       <DialogContent>
         <DialogHeader>
