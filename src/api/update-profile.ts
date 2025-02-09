5924060702af93246d98a8efd36777a084c48d35
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-02-09 16:46:12 -0300

    feat: create fn to call api

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -1,11 +1,13 @@
-import { api } from "@/lib/axios"
+import { api } from '@/lib/axios'
 
 interface UpdateProfileParams {
   name: string
-  description?: string 
+  description?: string
 }
 
-
-export default async function updateProfile({name, description}: UpdateProfileParams) {
-  await api.put('/profile', {name, description})
+export default async function updateProfile({
+  name,
+  description,
+}: UpdateProfileParams) {
+  await api.put('/profile', { name, description })
 }
