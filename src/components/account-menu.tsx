5c59d6a0ae4858fd2f9380613ba4b7d43c7764c3
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2025-02-09 16:47:29 -0300

    feat: create the logic to apply logout, using mutation and add a onSucess to navigate to sign-in url with replace true

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
@@ -16,8 +16,11 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 import { Skeleton } from './ui/skeleton'
+import { signOut } from '@/api/sign-out'
+import { useNavigate } from 'react-router-dom'
 
 function AccountMenu() {
+  const navigate = useNavigate()
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -29,6 +32,13 @@ function AccountMenu() {
       queryKey: ['managed-restaurant'],
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
+    })
+
+    const { mutateAsync: signOutFn, isPending: isSigningOut} = useMutation({
+      mutationFn: signOut,
+      onSuccess: () => {
+        navigate('/sign-in', { replace: true })
+      }
     })
 
   return (
@@ -70,11 +80,13 @@ function AccountMenu() {
               <span>Perfil da Loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Sair</span>
+          <DropdownMenuItem asChild className="text-rose-500 dark:text-rose-400" disabled={isSigningOut}>
+            <button className="flex w-full items-center" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span >Sair</span>
+            </button>
           </DropdownMenuItem>
-        </DropdownMenuContent>
+        </DropdownMenuContent> 
       </DropdownMenu>
 
       <StoreProfileDialog />
